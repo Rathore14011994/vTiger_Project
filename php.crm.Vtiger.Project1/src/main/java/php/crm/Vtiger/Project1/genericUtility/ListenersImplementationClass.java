0660723b51f7d0aa93f4e1be0406ad1562d5c763
shaurya <shaurya@shaurya-PC>
2022-10-26 12:53:51 +0530

    commit 7 pom.xml changed

--- php.crm.Vtiger.Project1/src/main/java/php/crm/Vtiger/Project1/genericUtility/ListenersImplementationClass.java
+++ php.crm.Vtiger.Project1/src/main/java/php/crm/Vtiger/Project1/genericUtility/ListenersImplementationClass.java
@@ -5,12 +5,13 @@ import java.io.IOException;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import com.google.common.io.Files;
 
-public class ListenersImplementationClass implements ITestListener {
+public  class ListenersImplementationClass implements ITestListener {
 	public void onTestFailure(ITestResult result) {
 		String testCaseName = result.getMethod().getMethodName();
 		TakesScreenshot ts=(TakesScreenshot)BaseClass.sDriver;
@@ -22,6 +23,36 @@ public class ListenersImplementationClass implements ITestListener {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+	}
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
 		
 	}
 	
